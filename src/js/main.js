fb6ba33f6e209530693bbfcf2d6ef5a69daa838c
Rasulback <ivannnurmin8@gmail.com>
2024-02-14 20:23:29 +0600

    first load

---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+// alert('hello world')
+//
+// prompt('what is your name', 'rasul')
+//
+// confirm('are you really sure')
+
+// переменные и их минусы плюсы
+// не должны использовать  var  всплытие
+// console.log(countPage);   //undefined
+// var countPage = 123093;
+// console.log(countPage);
+// const countPage = 20; //const не изменяемая переменная
+// countPage = 10;
+// console.log(countPage)
+// console.log(countPage);
+// countPage = 10;
+// console.log(countPage);
+
+// логические задачи
+// console.log(countPage2)
+// let countPage2 = 12;
+// console.log(countPage2)
+// let countPage = 123093;
+// let countPage = 100;
+// countPage = 5
+// a = countPage;
+// console.log(countPage);
+// console.log(a);
+
+
+
