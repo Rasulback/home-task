1cf1374eb9691e7f821550febf49554eb027ca17
Rasulback <ivannnurmin8@gmail.com>
2024-02-18 16:51:07 +0600

    first load

---
+++ src/js/main.js
@@ -0,0 +1,51 @@
+//1
+// console.log("My name is");
+//2
+// console.log("Micheal Jackson");
+//3
+// console.log("My name is: Simon");
+//4
+// console.log("Total cost: $" + (3 + 5));
+//5
+// console.log(`Total cost: $${3 + 5}`);
+//6
+// alert(`Total cost: $${3 + 5}`);
+//7
+// console.log("Total cost: " + ((599 + 299) / 100)); // катачылык бар окшойт 8.94 чыгыш учун 599 же 299 бироосу 595 же 295 болушу керек
+//8
+// console.log(`Total cost: $${(599 + 299) / 100}`); // катачылык бар окшойт 8.94 чыгыш учун 599 же 299 бироосу 595 же 295 болушу керек
+//9
+// alert(`Total cost: $${(599 + 299) / 100}. Thank you, come again!`); // у вас ошибка 8.94 чыгыш учун 599 же 299 бироосу 595 же 295 болушу керек
+//10
+// let itemsCost = 2 * 2095 + 2 * 799 / 100;
+// console.log(`Items (4): $${itemsCost}`);
+//11
+// console.log(`Shipping & handling: $${ (499 + 499) /100}`);
+//12
+// let totalBeforeTax = 2 * 2095 + 2 * 799  + 499 + 499 / 100;
+// console.log(`Total before tax: $${totalBeforeTax}`);
+//13
+// console.log(`Estimated tax (10%): $${Math.round((2 * 2095 + 2 * 499 + 499) * 0.1) / 100}`);
+
+
+//2
+//1
+// const name = 'Simon';
+//2
+// console.log(`My name is: ${name}`);
+//3
+// let cost = 5 + 2 * 3 + 9;
+//4
+// console.log(`Cost of food: $${cost}`);
+//5
+// let tax = cost * 0.1;
+//6
+// let totalCost = cost + tax;
+//7
+// console.log(`Total cost: $${totalCost}`);
+
+
+// 3
+let card = 0;
+
+
