2f3552d85e469f45c93a39a9585fb9d627108500
Rasulback <ivannnurmin8@gmail.com>
2024-02-19 16:19:31 +0600

    second load

--- src/js/main.js
+++ src/js/main.js
@@ -27,5 +27,36 @@
 // console.log(countPage);
 // console.log(a);
 
-
-
+// task1
+// alert("Good morning");
+// console.log("Good morning");
+// task2
+// alert("Micheal");
+// console.log("Michael");
+// ts3
+// document.body.innerHTML = "Good morning";
+// ts4
+// alert("Michael");
+// ts5
+// document.body.innerHTML = "<h1>Michael</h1>";
+// document.body.innerHTML = "";
+// ts6
+// let num = 6.4;
+// let roundedNum = Math.ceil(num);
+// console.log(roundedNum);
+// ts7
+// let num = 181.900;
+// let roundedNum = Math.floor(num);
+// console.log(roundedNum);
+// ts8
+// let num = 39.320;
+// let roundedNum = Math.trunc(num);
+// console.log(roundedNum);
+// ts9
+// let num1 = 18.6;
+// let num2 = 195.3;
+// let sum = num1 + num2;
+// let roundedSum = Math.round(sum);
+// console.log(roundedSum);
+// ts10
+// let generate = Math.random();
