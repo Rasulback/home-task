6d661a12a9ef3e1eee64d164bf20c66d1736e7dd
Rasulback <ivannnurmin8@gmail.com>
2024-02-19 15:40:48 +0600

    first load

---
+++ src/js/main.js
@@ -0,0 +1,57 @@
+// task1
+// let num = 10;
+// num === 10 ? console.log(true): console.log(false);
+// let a = 10;
+// a === 10 ? console.log('it\'s right'): console.log('it\'s wrong');
+//
+// task2
+// let apartment = prompt('Write your apartment number');
+// if (apartment <= 20 && apartment >= 1){
+//     alert('your entrance is first')
+// } else if (apartment <= 48 && apartment >= 21){
+//     alert('your entrance is second')
+// } else if (apartment <= 49 && apartment >= 90){
+//     alert('your entrance is third')
+// } else {
+//     alert('The apartment number is incorrect')
+// }
+//
+// task3
+// let a = 9;
+// if (a < 5 && a > 0){
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+//
+// task4
+// const hour = 9;
+// if (hour <= 12 && hour >= 6){
+//     console.log('Good morning')
+// } else if (hour <= 17 && hour >= 13){
+//     console.log('Good afternoon')
+// } else {
+//     console.log('Good evening')
+// }
+//
+// task5
+// const name = 'Thomas';
+// hour <= 12 && hour >= 6 ? console.log(`Good morning ${name}`) : (hour <= 17 && hour >= 13) ? console.log(`Good afternoon ${name}`) : console.log(`Good evening ${name}`);
+//
+// task6
+// const price = 5;
+// price >= 65 || price < 6 ? console.log('discount'): console.log('no discount');
+//
+// task7
+// let dot = Math.random()
+// dot > 0.5 ? console.log('cross') : console.log('pile');
+//
+// task8
+// const num = 10;
+// num % 2 === 0 ? console.log('event'): console.log('add'); // логика такая, если число делится на 2 без остатка, то оно четное, иначе нечетное
+//
+// task9
+// let num = 5;
+//     // num = 3;
+// num === 5 ? console.log('five') : console.log('not five');
+//
