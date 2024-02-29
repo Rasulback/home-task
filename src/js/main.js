6514fb6cae0af7a878fae3b8a88ae5ac836418e0
Rasulback <ivannnurmin8@gmail.com>
2024-02-29 19:17:19 +0600

    first load

---
+++ src/js/main.js
@@ -0,0 +1,48 @@
+// tsk1
+// let btnJs = document.querySelector('.js-button');
+//
+// console.log(btnJs.classList.contains('btn'));
+// console.log(btnJs.classList.contains('js-button'));
+
+// tsk2
+// function changeBtn() {
+//     let btn = document.querySelector('.subBtn');
+//     btn.classList.toggle('subBtn_active');
+//     btn.textContent = 'OFF';
+// }
+
+// tsk3
+// let input = document.querySelector('.inp-tsk3');
+// let btn = document.querySelector('.btn-tsk3');
+// let txt = document.querySelector('.txt-tsk3');
+// function showResult() {
+//     txt.textContent = `Your name is: ${input.value}`;
+//     input.value = '';
+// }
+
+// tsk4
+// input.addEventListener('keydown', function(e) {
+//     if (e.key === 'Enter') {
+//         showResult();
+//     }
+// });
+
+// tsk5
+// let inp = document.querySelector('.inp-tsk5');
+// let txt = document.querySelector('.txt-tsk5');
+// inp.addEventListener('input', function() {
+//     txt.textContent = inp.value;
+// });
+
+// tsk6
+// let txt = document.querySelector('.txt-tsk6');
+// function addTextInTxt(value) {
+//     txt.textContent += value;
+//     // Сохранение значения при каждом изменении
+//     localStorage.setItem('txtContent', txt.textContent);
+// }
+// // Восстановление значения при загрузке страницы
+//     txt.textContent = localStorage.getItem('txtContent');
+
+// tsk7
+// tsk7 in src\images
