e5baffd16db80f347158c46db30098f808a025ab
Rasulback <ivannnurmin8@gmail.com>
2024-03-08 17:27:07 +0600

    first

---
+++ src/js/main.js
@@ -0,0 +1,36 @@
+// tsk1
+// const user = {
+//     id: 1,
+//     first_name: "Garrott",
+//     last_name: "Eberdt",
+//     email: "geberdt0@wp.com",
+//     gender: "Male",
+//     ip_address: "49.230.15.110",
+//     isAdmin: true,
+// };
+//
+// const { id, first_name, last_name, email, gender, ip_address, isAdmin } = user;
+
+// tsk2
+// const animals = ["apple", "banana", "lemon", "kiwi", true];
+// const [apple, banana, lemon, kiwi, kiwi2] = animals;
+// console.log(apple, banana, lemon, kiwi, kiwi2);
+
+// tsk3
+
+
+// tsk4
+// const add = function() {
+//   console.log(2 + 3);
+// };
+//
+// add();
+// add();
+//
+// function runTwice(fun) {
+//   fun();
+//   fun();
+// }
+
+
+
